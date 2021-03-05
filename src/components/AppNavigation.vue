c3c62456692afea69feca926f0b59ce5456a6949
Marcelo Terenciani <marcelo.terenciani@gmail.com>
2021-03-05 08:18:34 -0300

    Correções das cores da barra de navegação

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,36 +1,32 @@
 <template>
     <span>
-        <v-navigation-drawer
-            app
-            v-model="drawer"
-            class="brown lighten-2"
-            dark
-            disable-resize-watcher
-        >
+        <v-navigation-drawer app v-model="drawer" disable-resize-watcher>
             <v-list>
-                <template v-for="(item, index) in items">
-                    <v-list-tile :key="index">
-                        <v-list-tile-content>
-                            {{ item.title }}
-                        </v-list-tile-content>
-                    </v-list-tile>
+                <v-list-item v-for="(item, index) in items" :key="index">
+                    <v-list-item-title>
+                        {{ item.title }}
+                    </v-list-item-title>
                     <v-divider :key="`divider-${index}`"></v-divider>
-                </template>
+                </v-list-item>
             </v-list>
         </v-navigation-drawer>
-        <v-toolbar app color="brown darken-4" dark>
-            <v-toolbar-side-icon
+        <v-toolbar dark>
+            <v-app-bar-nav-icon
                 class="hidden-md-and-up"
                 @click="drawer = !drawer"
-            ></v-toolbar-side-icon>
+            ></v-app-bar-nav-icon>
             <v-spacer class="hidden-md-and-up"></v-spacer>
             <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
-            <v-btn flat class="hidden-sm-and-down">Menu</v-btn>
+            <v-toolbar-items>
+                <v-btn text class="hidden-sm-and-down">Menu</v-btn>
+            </v-toolbar-items>
             <v-spacer class="hidden-sm-and-down"></v-spacer>
-            <v-btn flat class="hidden-sm-and-down">SIGN IN</v-btn>
-            <v-btn color="brown lighten-3" class="hidden-sm-and-down"
-                >JOIN</v-btn
-            >
+            <v-toolbar-items>
+                <v-btn text class="hidden-sm-and-down">SIGN IN</v-btn>
+                <v-btn text class="grey darken-3 hidden-sm-and-down"
+                    >JOIN</v-btn
+                >
+            </v-toolbar-items>
         </v-toolbar>
     </span>
 </template>
@@ -40,7 +36,7 @@ export default {
     name: 'AppNavigation',
     data() {
         return {
-            appTitle: 'Meal Prep',
+            appTitle: 'Hectare Maps',
             drawer: false,
             items: [{ title: 'Menu' }, { title: 'Sign In' }, { title: 'Join' }]
         };
