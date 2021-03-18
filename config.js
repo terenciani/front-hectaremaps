5376de61d48461e6a23b838667fb8b93289e2596
Marcelo Terenciani <marcelo.terenciani@gmail.com>
2021-03-18 11:55:48 -0300

    Mudança do protocolo HTTP

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
-    apiHost: 'https://hectaremaps-api.herokuapp.com',
-    // apiHost: 'http://localhost:3000',
+    apiHost: 'http://hectaremaps-api.herokuapp.com',
+    //apiHost: 'http://localhost:3000',
     privateArea: 'https://hectaremaps-admin.herokuapp.com',
     decimalPlaces: 2
 };
