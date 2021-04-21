c8d19bb9a40e22647fad37fd19bd52804b0aa6a5
Marcelo Terenciani <marcelo.terenciani@gmail.com>
2021-04-21 15:13:44 -0300

    correção da rota de configuração

--- src/service/DataService.js
+++ src/service/DataService.js
@@ -5,7 +5,7 @@ import API from '../Api';
 export default class DataService {
     static async getData() {
         try {
-            let siteData = await API.get('sitedata');
+            let siteData = await API.get('configdata');
             return siteData.data;
         } catch (error) {
             throw error.response.data;
